8ace9ba1e86c475a6e0848afb190b4b406678d15
= <mutkumese@gmail.com>
2024-05-30 21:21:33 +0300

    Add search video with tag feature

--- lib/controllers/video_controller.dart
+++ lib/controllers/video_controller.dart
@@ -5,8 +5,11 @@ import '../utils/constants.dart';
 
 class VideoController extends GetxController {
   final Rx<List<Video>> _videoList = Rx<List<Video>>([]);
+  final Rx<List<Video>> _searchedVideos = Rx<List<Video>>([]);
+
   List<Video> get videoList =>
       _videoList.value.where((video) => video.isVisible).toList();
+  List<Video> get searchedVideos => _searchedVideos.value;
 
   @override
   void onInit() {
@@ -26,7 +29,27 @@ class VideoController extends GetxController {
     }));
   }
 
-  likeVideo(String videoId) async {
+  void searchVideosByTag(String tag) {
+    _searchedVideos.bindStream(firestore
+        .collection("videos")
+        .where("tag", isEqualTo: tag)
+        .snapshots()
+        .map((QuerySnapshot query) {
+      List<Video> retVal = [];
+      for (var element in query.docs) {
+        retVal.add(
+          Video.fromSnap(element),
+        );
+      }
+      return retVal;
+    }));
+  }
+
+  void clearSearchedVideos() {
+    _searchedVideos.value = [];
+  }
+
+  Future<void> likeVideo(String videoId) async {
     DocumentSnapshot doc =
         await firestore.collection("videos").doc(videoId).get();
 
--- lib/views/screens/search_screen.dart
+++ lib/views/screens/search_screen.dart
@@ -1,33 +1,45 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../controllers/search_controller.dart';
+import '../../controllers/video_controller.dart';
 import '../../models/user_model.dart';
+import '../../models/video_model.dart';
 import 'profile_screen.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   SearchScreen({super.key});
 
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
   final MySearchController searchController = Get.put(MySearchController());
-  final TextEditingController _textEditingController = TextEditingController();
+
+  final VideoController videoController = Get.put(VideoController());
+
+  final TextEditingController _searchController = TextEditingController();
+
+  final RxBool isVideoSearch = false.obs;
 
   @override
   Widget build(BuildContext context) {
     FlutterStatusbarcolor.setStatusBarColor(Colors.deepPurpleAccent.shade100);
-    final size = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: AppBar(
         title: TextFormField(
-          controller: _textEditingController,
+          controller: _searchController,
           style: GoogleFonts.poppins(
             fontSize: 20,
             color: Colors.white,
           ),
           decoration: InputDecoration(
-            hintText: 'Search',
+            hintText: isVideoSearch.value ? "Search Videos" : "Search Users",
             hintStyle: GoogleFonts.poppins(
               fontSize: 20,
               color: Colors.white,
@@ -36,98 +48,106 @@ class SearchScreen extends StatelessWidget {
             suffixIcon: IconButton(
               icon: Icon(Icons.clear, color: Colors.white),
               onPressed: () {
-                _textEditingController.clear();
+                _searchController.clear();
                 searchController.clearSearch();
+                videoController.clearSearchedVideos();
               },
             ),
           ),
-          onFieldSubmitted: (value) => searchController.searchUser(value),
+          onFieldSubmitted: (value) {
+            if (isVideoSearch.value) {
+              videoController.searchVideosByTag(value);
+            } else {
+              searchController.searchUser(value);
+            }
+          },
         ),
         backgroundColor: Colors.deepPurpleAccent.shade100,
         elevation: 0,
         actions: [
-          IconButton(
-            onPressed: () =>
-                searchController.searchUser(_textEditingController.text),
-            icon: const Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-          ),
+          Obx(() {
+            return Row(
+              children: [
+                Switch(
+                  value: isVideoSearch.value,
+                  onChanged: (value) {
+                    setState(() {
+                      isVideoSearch.value = value;
+                    });
+                    _searchController.clear();
+                    searchController.clearSearch();
+                    videoController.clearSearchedVideos();
+                  },
+                  activeColor: Colors.white,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.search, color: Colors.white),
+                  onPressed: () {
+                    if (isVideoSearch.value) {
+                      videoController.searchVideosByTag(_searchController.text);
+                    } else {
+                      searchController.searchUser(_searchController.text);
+                    }
+                  },
+                ),
+              ],
+            );
+          }),
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Obx(() {
-              if (searchController.searchedUsers.isEmpty &&
-                  _textEditingController.text.isEmpty) {
-                return ListView(
-                  padding: EdgeInsets.all(16),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Obx(() {
+                if (!isVideoSearch.value &&
+                    searchController.searchedUsers.isEmpty &&
+                    _searchController.text.isEmpty) {
+                  return _buildSearchHistory();
+                }
+
+                if (isVideoSearch.value &&
+                    videoController.searchedVideos.isEmpty &&
+                    _searchController.text.isEmpty) {
+                  return _buildSearchHistory();
+                }
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    _buildSearchHistorySection(
-                      title: "Recent Searches",
-                      searches: searchController.recentSearches,
-                      onClear: searchController.clearRecentSearches,
-                    ),
-                    SizedBox(height: 20),
-                    _buildSearchHistorySection(
-                      title: "Frequent Searches",
-                      searches: searchController.frequentSearches,
-                      onClear: searchController.clearFrequentSearches,
-                    ),
+                    if (!isVideoSearch.value &&
+                        searchController.searchedUsers.isNotEmpty)
+                      _buildUserResults(searchController.searchedUsers),
+                    if (isVideoSearch.value &&
+                        videoController.searchedVideos.isNotEmpty)
+                      _buildVideoResults(videoController.searchedVideos),
                   ],
                 );
-              }
-
-              if (searchController.searchedUsers.isEmpty) {
-                return Center(
-                  child: Text(
-                    "No users found.",
-                    style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      color: Colors.black,
-                    ),
-                  ),
-                );
-              }
-
-              return ListView.builder(
-                itemCount: searchController.searchedUsers.length,
-                itemBuilder: (context, index) {
-                  User user = searchController.searchedUsers[index];
-                  return InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => ProfileScreen(
-                            uid: user.uid,
-                          ),
-                        ),
-                      );
-                    },
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.grey,
-                        backgroundImage: NetworkImage(
-                          user.profilePhoto,
-                        ),
-                      ),
-                      title: Text(
-                        user.name,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
-            }),
+              }),
+            ],
           ),
-        ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildSearchHistory() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildSearchHistorySection(
+          title: "Recent Searches",
+          searches: searchController.recentSearches,
+          onClear: searchController.clearRecentSearches,
+        ),
+        SizedBox(height: 20),
+        _buildSearchHistorySection(
+          title: "Frequent Searches",
+          searches: searchController.frequentSearches,
+          onClear: searchController.clearFrequentSearches,
+        ),
+      ],
     );
   }
 
@@ -167,8 +187,12 @@ class SearchScreen extends StatelessWidget {
             children: searches.map((search) {
               return GestureDetector(
                 onTap: () {
-                  _textEditingController.text = search;
-                  searchController.searchUser(search);
+                  _searchController.text = search;
+                  if (isVideoSearch.value) {
+                    videoController.searchVideosByTag(search);
+                  } else {
+                    searchController.searchUser(search);
+                  }
                 },
                 child: Chip(
                   label: Text(
@@ -186,5 +210,107 @@ class SearchScreen extends StatelessWidget {
         ],
       );
     });
+  }
+
+  Widget _buildUserResults(List<User> users) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "User Results",
+          style: GoogleFonts.poppins(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            User user = users[index];
+            return InkWell(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(
+                      uid: user.uid,
+                    ),
+                  ),
+                );
+              },
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  backgroundImage: NetworkImage(
+                    user.profilePhoto,
+                  ),
+                ),
+                title: Text(
+                  user.name,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildVideoResults(List<Video> videos) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Video Results",
+          style: GoogleFonts.poppins(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: videos.length,
+          itemBuilder: (context, index) {
+            Video video = videos[index];
+            return ListTile(
+              leading: CachedNetworkImage(
+                imageUrl: video.thumbnail,
+              ),
+              title: Text(
+                video.caption,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Text(
+                "by ${video.username}",
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(
+                      uid: video.username,
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ],
+    );
   }
 }
